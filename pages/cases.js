31addab256b49413541fd0e30731b6c10243bbfd
Nollee <normannmail@gmail.com>
2020-05-28 17:20:32 +0200

    pages og components

---
+++ pages/cases.js
@@ -0,0 +1,29 @@
+export default class Cases {
+    constructor() {
+      this.template();
+    }
+  
+    template() {
+      document.querySelector("#cases").innerHTML += /*html*/ `
+       
+    <div class="swiper-container swiper1">
+    <!-- Additional required wrapper -->
+    <div id="caseinfo" class="overlay">
+    </div>
+    <div id="slides" class="swiper-wrapper">
+        <!-- Slides -->
+
+    </div>
+    <!--
+
+    <div class="swiper-pagination1"></div>
+
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+
+    <div class="swiper-scrollbar"></div>
+    -->
+</div>
+      `;
+    }
+  }
